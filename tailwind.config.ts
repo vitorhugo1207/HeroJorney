607d481f9fae0c426da861d7d9b7be6e7faf5be0
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-11-04 20:00:58 -0300

    chore: added Inter var Font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,13 @@
-import type { Config } from 'tailwindcss'
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+module.exports = {
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  // ...
 }
-export default config
