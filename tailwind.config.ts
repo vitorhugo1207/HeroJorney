8db32b619ea30f5e9dc6a6d85c19dd5d4a133546
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-11-05 11:25:21 -0300

    raw: fixed tailwind doesn't working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
