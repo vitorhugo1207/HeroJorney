8064d1451cc28afaf0f4358c2bd1468589dd10f6
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-11-05 14:11:06 -0300

    feat: added all heros visualizations

--- src/app/page.js
+++ src/app/page.js
@@ -7,63 +7,87 @@ import Card from '@mui/joy/Card';
 import CardCover from '@mui/joy/CardCover';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import '../app/card.css';
 
+function Cards({ herosData }) {
+	function cardModel(hero) {
+		return (
+			<div className='overflow-hidden rounded-lg' key={hero?.id}>
+        <Card
+          sx={{
+            minHeight: '340px',
+            width: '250px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: '2px',
+            borderColor: '#0f172a',
+          }}
+          className='card'>
+          <CardCover>
+            <Image
+              src={hero?.images?.lg}
+              fill
+              priority={true}
+              alt={hero?.name}
+              sizes='(max-width: 340px) 100vw, (max-width: 280px) 50vw, 33vw'
+            />
+          </CardCover>
+          <CardCover
+            sx={{
+              background:
+                'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
+            }}
+          />
+          <CardContent
+            sx={{ justifyContent: 'flex-end' }}
+            className='text-center'>
+            <Typography level='title-lg' textColor='#fff'>
+              {hero?.name}
+            </Typography>
+            <Typography textColor='neutral.300'>
+              {hero?.biography?.publisher}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+		);
+	}
+
+	function genCards() {
+		let cards = [];
+
+		for (const hero of herosData) {
+			cards.push(cardModel(hero));
+		}
+
+		return cards;
+	}
+
+	return genCards();
+}
+
 export default function Home() {
-	const [herosData, setHerosData] = useState();
+	const [herosData, setHerosData] = useState([]);
+
+	async function getHerosData() {
+		const heros = new Heros();
+		const response = await heros.getHeros();
+		return response;
+	}
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const heros = new Heros();
-			const response = await heros.getHeros();
-			setHerosData(response);
+			const data = await getHerosData();
+			setHerosData(data);
 		};
-
 		fetchData();
 	}, []);
 
 	return (
 		<>
-			<div className='flex justify-around p-10'>
-				<div className='overflow-hidden rounded-lg'>
-          <Card
-            sx={{
-              minHeight: '340px',
-              width: '250px',
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: '2px',
-              borderColor: '#0f172a',
-            }}
-            className='card'>
-            <CardCover >
-              <Image
-                src='https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/1-a-bomb.jpg'
-                fill
-                priority={true}
-                alt='test'
-                sizes='(max-width: 340px) 100vw, (max-width: 280px) 50vw, 33vw'
-              />
-            </CardCover>
-            <CardCover 
-              sx={{
-                background:
-                  'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
-              }}
-            />
-            <CardContent sx={{ justifyContent: 'flex-end'}} className='text-center'>
-              <Typography level='title-lg' textColor='#fff'>
-                A-Bomb
-              </Typography>
-              <Typography
-                textColor='neutral.300'>
-                Marvel Comics
-              </Typography>
-            </CardContent>
-          </Card>
-        </div>
-			</div>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-6 p-10'>
+        <Cards herosData={herosData} />
+      </div>
 		</>
 	);
 }
