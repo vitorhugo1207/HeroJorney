ce2c24aae2fdc45ed39e3982869a8f7532926a11
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-11-05 18:09:33 -0300

    feat: added Search function

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,53 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { Search, Label } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import _ from 'lodash';
+import Link from 'next/link';
+import Heros from '@/API/Heroes';
+
 export default function Home() {
+	const [results, setResults] = useState([]);
+	const [value, setValue] = useState('');
+	const [herosData, setHerosData] = useState([]);
+
+	async function getHerosData() {
+		const heros = new Heros();
+		const response = await heros.getHeros();
+		return response;
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await getHerosData();
+			setHerosData(data);
+		};
+		fetchData();
+	}, []);
+
+	const handleSearchChange = e => {
+		let value = e.target.value;
+		setValue(value);
+		const re = new RegExp(_.escapeRegExp(value), 'i');
+		const isMatch = (result) => re.test(result.name);
+		setResults(_.filter(herosData, isMatch));
+	}
+
+	const resultRenderer = ({ name }) => {
+		console.log(name);
+		return <Label content={name} />
+	}
+
 	return (
-    <>
-      <p>Home Page in Construction!</p>
-	</>
+		<>
+			<Search
+				onSearchChange={handleSearchChange}
+				noResultsMessage='Hero no found.'
+				resultRenderer={resultRenderer}
+				results={results}
+				value={value}
+			/>
+		</>
 	);
 }
