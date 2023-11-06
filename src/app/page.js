9112d0b4e855db2e7b35d50cca8707a87973dad8
vitorhugo1207 <vitorhugoalvesferreira@outlook.com>
2023-11-06 09:41:00 -0300

    feat and refator: added style to Search

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,54 @@
 
 import { useEffect, useState } from 'react';
 import { Search, Label } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
 import _ from 'lodash';
-import Link from 'next/link';
 import Heros from '@/API/Heroes';
 import Image from 'next/image';
+import 'semantic-ui-css/semantic.min.css';
+import './results.css';
 
-export default function Home() {
+function SearchBar({herosData}) {
 	const [results, setResults] = useState([]);
 	const [value, setValue] = useState('');
+
+	const handleSearchChange = (e) => {
+		let value = e.target.value;
+		setValue(value);
+		const re = new RegExp(_.escapeRegExp(value), 'i');
+		const isMatch = (result) => re.test(result.name);
+		setResults(_.filter(herosData, isMatch));
+	};
+
+	const resultRenderer = ({ name, images }) => {
+		return (
+			<div className='grid grid-cols-2 items-center'>
+				<Image
+					src={images.md}
+					priority={true}
+					alt={name}
+					width={50}
+					height={50}
+					style={{ width: 'auto', height: 'auto' }}
+				/>
+				<Label content={name}></Label>
+			</div>
+		);
+	};
+
+	return (
+		<div className='grid grid-cols-1 justify-items-center p-10'>
+			<Search
+				onSearchChange={handleSearchChange}
+				noResultsMessage='Hero no found.'
+				resultRenderer={resultRenderer}
+				results={results}
+				value={value}
+			/>
+		</div>
+	);
+}
+
+export default function Home() {
 	const [herosData, setHerosData] = useState([]);
 
 	async function getHerosData() {
@@ -27,39 +66,7 @@ export default function Home() {
 		fetchData();
 	}, []);
 
-	const handleSearchChange = (e) => {
-		let value = e.target.value;
-		setValue(value);
-		const re = new RegExp(_.escapeRegExp(value), 'i');
-		const isMatch = (result) => re.test(result.name);
-		setResults(_.filter(herosData, isMatch));
-	};
-
-	const resultRenderer = ({ name, images }) => {
-		return (
-			<div>
-				<Image
-					src={images.md}
-					priority={true}
-					alt={name}
-					width={50}
-					height={50}
-					style={{ width: "auto", height: "auto" }}
-				/>
-				<p className='relative text-right align-top'>{name}</p>
-			</div>
-		);
-	};
-
 	return (
-		<>
-			<Search
-				onSearchChange={handleSearchChange}
-				noResultsMessage='Hero no found.'
-				resultRenderer={resultRenderer}
-				results={results}
-				value={value}
-			/>
-		</>
+		<SearchBar herosData={herosData} />
 	);
 }
